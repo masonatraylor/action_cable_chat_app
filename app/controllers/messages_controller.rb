7c70e19203e169c5fa87fd0782c47b889e1202a4
Mason <mason.a.traylor@gmail.com>
2019-12-05 15:25:06 -1000

    Render messages with partial

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,19 +9,23 @@ class MessagesController < ApplicationController
     message = current_user.messages.build(message_params)
     if message.save
       ActionCable.server.broadcast 'room_channel',
-                                   content:  message.content,
-                                   username: message.user.username
+                                    message: render_message(message)
+
     end
   end
 
   private
 
-    def get_messages
-      @messages = Message.for_display
-      @message  = current_user.messages.build
-    end
+  def get_messages
+    @messages = Message.for_display
+    @message  = current_user.messages.build
+  end
 
-    def message_params
-      params.require(:message).permit(:content)
-    end
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def render_message(message)
+    render(partial: 'message', locals: { message: message })
+  end
 end
