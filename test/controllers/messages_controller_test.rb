1cf5cd71074de23ec69171bf4e113a9b7b16a1c4
Mason <mason.a.traylor@gmail.com>
2019-12-05 14:59:24 -1000

    Make tests pass

--- test/controllers/messages_controller_test.rb
+++ test/controllers/messages_controller_test.rb
@@ -19,13 +19,13 @@ class MessagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should handle invalid message" do
     login_as @user
-    post messages_url, params: { message: { content: "" } }
+    post messages_url, xhr: true, params: { message: { content: "" } }
     assert_response :success
   end
 
   test "should create valid message" do
     login_as @user
-    post messages_url, params: { message: { content: "Lorem" } }
-    assert_response :redirect
+    post messages_url, xhr: true, params: { message: { content: "Lorem" } }
+    assert_response :success
   end
 end
